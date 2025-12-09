db54bdc4dd5dd0e78e048710f86282e297df86aa
sejjy <sejjymvm@gmail.com>
2025-12-09 22:26:41 +0800

    chore: initial commit

---
+++ email.js
@@ -0,0 +1,21 @@
+// https://www.emailjs.com
+
+emailjs.init({
+	publicKey: "PUBLIC_KEY",
+});
+
+document
+	.getElementById("auth-form")
+	.addEventListener("submit", function (event) {
+		event.preventDefault();
+		emailjs.sendForm("SERVICE_ID", "TEMPLATE_ID", this).then(
+			() => {
+				console.log("SUCCESS!");
+				alert("Verification link sent! Please check your email.");
+			},
+			(error) => {
+				console.log("FAILED...", error);
+				alert("Error sending email. Please try again later.");
+			},
+		);
+	});
